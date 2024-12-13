448ec568b708c409e564fcd42d7951bc549b79a9
wangxuechee <156255648+wangxuechee@users.noreply.github.com>
2024-12-13 09:32:39 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -41,11 +41,3 @@ app.use(errorHandler);
 // Start the server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-const path = require('path');
-
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-});
